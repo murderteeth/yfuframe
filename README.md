1f1fadedce6f4fdc3e159c8519db61d64bcb41b7
murderteeth <murdertxxth@gmail.com>
2024-03-24 23:36:22 +0000

    🖼️

---
+++ README.md
@@ -0,0 +1,6 @@
+```
+bun install
+bun run dev
+```
+
+Head to http://localhost:5173/api
---
+++ api/index.tsx
@@ -0,0 +1,74 @@
+import { Button, Frog } from 'frog'
+import { devtools } from 'frog/dev'
+import { serveStatic } from 'frog/serve-static'
+// import { neynar } from 'frog/hubs'
+import { handle } from 'frog/vercel'
+
+// Uncomment to use Edge Runtime.
+// export const config = {
+//   runtime: 'edge',
+// }
+
+export const app = new Frog({
+  assetsPath: '/',
+  basePath: '/api',
+  // Supply a Hub to enable frame verification.
+  // hub: neynar({ apiKey: 'NEYNAR_FROG_FM' })
+})
+
+app.frame('/', (c) => {
+  const { buttonValue, inputText, status } = c
+  const fruit = inputText || buttonValue
+  return c.res({
+    image: (
+      <div
+        style={{
+          alignItems: 'center',
+          background:
+            status === 'response'
+              ? 'linear-gradient(to right, #432889, #17101F)'
+              : 'black',
+          backgroundSize: '100% 100%',
+          display: 'flex',
+          flexDirection: 'column',
+          flexWrap: 'nowrap',
+          height: '100%',
+          justifyContent: 'center',
+          textAlign: 'center',
+          width: '100%',
+        }}
+      >
+        <div
+          style={{
+            color: 'white',
+            fontSize: 60,
+            fontStyle: 'normal',
+            letterSpacing: '-0.025em',
+            lineHeight: 1.4,
+            marginTop: 30,
+            padding: '0 120px',
+            whiteSpace: 'pre-wrap',
+          }}
+        >
+          {status === 'response'
+            ? `Nice choice.${fruit ? ` ${fruit.toUpperCase()}!!` : ''}`
+            : 'Welcome!'}
+        </div>
+      </div>
+    ),
+    intents: [
+      <Button value="apples">Apples</Button>,
+      <Button value="oranges">Oranges</Button>,
+      <Button.Link href="https://zora.co/collect/zora:0x55f5a5d980992e01256d86e7ef03a22fd5fe84af/1">Mint</Button.Link>,
+      status === 'response' && <Button.Reset>Reset</Button.Reset>,
+    ],
+  })
+})
+
+// @ts-ignore
+const isEdgeFunction = typeof EdgeFunction !== 'undefined'
+const isProduction = isEdgeFunction || import.meta.env?.MODE !== 'development'
+devtools(app, isProduction ? { assetsPath: '/.frog' } : { serveStatic })
+
+export const GET = handle(app)
+export const POST = handle(app)
